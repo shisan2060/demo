1d6b794cf5c251ea95ea111e1eb43824dfcc568f
shisan2060 <shisan2060@dingtalk.com>
2017-03-31 15:01:34 +0800

    TestController 注释修改

--- src/main/java/com/jy/demo/controller/TestController.java
+++ src/main/java/com/jy/demo/controller/TestController.java
@@ -5,23 +5,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * Created by shisan on 2017/3/30.
+ * @Description 测试Controller
+ *
+ * @Author shisan
+ * @Date 2017/3/31 下午2:52
  */
 @Controller
 @RequestMapping("/testController")
 public class TestController {
 
     /**
-     * getData
+     * 获取数据
      *
-     * @param a
-     * @param b
+     * @param a 参数a
+     * @param b 参数b
      * @author shisan
      * @date 2017/3/31 下午2:41
      */
     @RequestMapping("/getData")
     @ResponseBody
     public String getData(int a, String b) {
+        System.out.println(a+b);
         return "org.springframework.web.bind.annotation.ResponseBody";
     }
 }
